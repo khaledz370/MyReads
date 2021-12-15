00b29ec4ebf8edfa143b746f423dc5f251491675
khaled2trust <76612087+khaled2trust380@users.noreply.github.com>
2021-12-15 02:12:02 +0200

    handle change category

--- src/Category.js
+++ src/Category.js
@@ -1,40 +1,46 @@
 import React, { Component } from 'react'
+
 /*eslint-disable */
-class WantToRead extends Component {
+class Category extends Component {
+    changeCategory = (bookId,event) => {
+        let category = event.target.value
+        this.props.onChangeCategory([bookId,category])
+    }
     render() {
         return (
             <div className="bookshelf">
-                <h2 className="bookshelf-title">Want to Read</h2>
+                <h2 className="bookshelf-title">{this.props.category}</h2>
+                
                 <div className="bookshelf-books">
                     <ol className="books-grid">
-                            {typeof this.props.books !== "undefined" ?  Object.entries(this.props.books).map((book) => {
-                                if (book[1].shelf === 'wantToRead') {
-                                    return (
-                                        <li key={book[1].id}>
+                        {typeof this.props.books !== "undefined" ? Object.entries(this.props.books).map((book) => {
+                            if (book[1].shelf === this.props.categoryName) {
+                                return (
+                                    <li key={book[1].id}>
                                         <div className="book">
                                             <div className="book-top">
                                                 <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book[1].imageLinks.thumbnail}")` }}></div>
                                                 <div className="book-shelf-changer">
-                                                    <select defaultValue={"wantToRead"}>
+                                                    <select value={this.props.categoryName} onChange={(value)=>{this.changeCategory(book[1],event)}}>
                                                         <option value="move" disabled>Move to...</option>
                                                         <option value="currentlyReading">Currently Reading</option>
                                                         <option value="wantToRead">Want to Read</option>
                                                         <option value="read">Read</option>
-                                                        <option value="none">None</option>
+                                                        <option value="none" disabled>None</option>
                                                     </select>
                                                 </div>
                                             </div>
                                             <div className="book-title">{book[1].title}</div>
                                             <div className="book-authors">{book[1].authors}</div>
                                         </div>
-                                        </li>
-                                    )
-                                }
-                            }) : ''}
+                                    </li>
+                                )
+                            }
+                        }) : ''}
                     </ol>
                 </div>
             </div>
         )
     }
 }
-export default WantToRead;
+export default Category;
--- src/LIstBooks.js
+++ src/LIstBooks.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
-import CurrentlyReading from './CurrentlyReading';
-import Read from './Read';
-import WantToRead from './WantToRead';
 import * as BooksAPI from './BooksAPI'
+import Category from './Category';
 
 class ListBooks extends Component {
   state = {}
@@ -13,6 +11,14 @@ class ListBooks extends Component {
       this.setState({ books: data })
     )
   }
+  changeCategory = (item) => {
+    BooksAPI.getAll()
+    BooksAPI.update(item[0], item[1])
+
+    BooksAPI.getAll().then(data =>
+      this.setState({ books: data }))
+  }
+
   render() {
     return (
       <div className="list-books">
@@ -21,9 +27,13 @@ class ListBooks extends Component {
         </div>
         <div className="list-books-content">
           <div>
-            <CurrentlyReading books={typeof this.state.books !== 'undefined' ?this.state.books:''}/>
-            <WantToRead books={typeof this.state.books !== 'undefined' ?this.state.books:''}/>
-            <Read books={typeof this.state.books !== 'undefined' ?this.state.books:''}/>
+
+            <Category books={typeof this.state.books !== 'undefined' ? this.state.books : ''} onChangeCategory={this.changeCategory} category={'Currently Reading'} categoryName={'currentlyReading'} />
+
+            <Category books={typeof this.state.books !== 'undefined' ? this.state.books : ''} onChangeCategory={this.changeCategory} category={'Want to Read'} categoryName={'wantToRead'} />
+
+            <Category books={typeof this.state.books !== 'undefined' ? this.state.books : ''} onChangeCategory={this.changeCategory} category={'Read'} categoryName={'read'} />
+
           </div>
         </div>
         <div className="open-search">
